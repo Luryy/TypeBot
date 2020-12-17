e7b8cc86ec2fe815f07ca76100443eccdd73fe0a
Lucas Yuri <lucasyuri56@hotmail.com>
2020-12-17 19:10:47 -0300

    move server to infra

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import http from 'http';
-
-var port = process.env.PORT || 3000;
-
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify({ name: 'teste-bot', ver: '1.0.0' }));
-  res.end();
-}).listen(port)
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const port = process.env.PORT || 3000;
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ name: 'teste-bot', ver: '1.0.0' }));
+    res.end();
+  })
+  .listen(port);
