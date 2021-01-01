320d3576fc063c8b645fc4a9105f55f7f3bacf2b
Lucas Yuri <lucasyuri56@hotmail.com>
2021-01-01 17:55:41 -0300

    fix: database configirations

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,8 +2,8 @@ export default {
   postgres: {
     host: process.env.POSTGRES_HOST,
     port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_USERNAME,
-    database: process.env.POSTGRES_PASSWORD,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
   },
 };
